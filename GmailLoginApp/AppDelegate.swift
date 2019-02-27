30af23f83658ef3f7506b51840c7a6b655ed82f0
Tyler Percy <tpercy@ironman.local>
2019-02-27 13:40:39 -0500

    Login Completed

--- GmailLoginApp/AppDelegate.swift
+++ GmailLoginApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        GIDSignIn.sharedInstance().clientID =
+                "260486845262-09l5el3a1r4q1bvqo5ajs7vv39bdnhvk.apps.googleusercontent.com"
         return true
     }
 
--- GmailLoginApp/ViewController.swift
+++ GmailLoginApp/ViewController.swift
@@ -7,12 +7,67 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            print("\(error.localizedDescription)")
+        } else {
+            // Perform any operations on signed in user here.
+            let userId = user.userID // For client-side use only!
+            let idToken = user.authentication.idToken // Safe to send to the server
+            let fullName = user.profile.name
+            let givenName = user.profile.givenName
+            let familyName = user.profile.familyName
+            let email = user.profile.email
+            print(userId! ," ",idToken!," ",fullName!," ",givenName!," ",familyName!," ",email!)
+            statusText.text = fullName!
+            toggleAuthUI()
+        }
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+              withError error: Error!) {
+    }
+    
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    @IBOutlet weak var signOutButton: UIButton!
+    @IBOutlet weak var disconnectButton: UIButton!
+    @IBOutlet weak var statusText: UILabel!
+    
+    func toggleAuthUI() {
+        if GIDSignIn.sharedInstance().hasAuthInKeychain() {
+            // Signed in
+            signInButton.isHidden = true
+            signOutButton.isHidden = false
+            disconnectButton.isHidden = false
+        } else {
+            signInButton.isHidden = false
+            signOutButton.isHidden = true
+            disconnectButton.isHidden = true
+            statusText.text = "Google Sign in\niOS Demo"
+        }
+    }
+    
+    @IBAction func didTapSignOut(_ sender: AnyObject) {
+        GIDSignIn.sharedInstance().signOut()
+        statusText.text = "Signed out."
+        toggleAuthUI()
+    }
+    
+    @IBAction func didTapDisconnect(_ sender: AnyObject) {
+        GIDSignIn.sharedInstance().disconnect()
+        statusText.text = "Disconnecting."
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        
+        toggleAuthUI()
     }
 
     override func didReceiveMemoryWarning() {
